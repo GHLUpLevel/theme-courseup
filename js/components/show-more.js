39c8f754e7617c80fd7155809d97aee074371ed4
Joe Johnston <104495+simple10@users.noreply.github.com>
2024-03-13 19:08:26 -0700

    v1.2.1

--- js/components/show-more.js
+++ js/components/show-more.js
@@ -1,7 +1,7 @@
 /****************************************
  * Level Up Theme for High Level (GHL)
  * https//highlevelthemes.com
- * Version: v1.2.0
+ * Version: v1.2.1
  ****************************************/
 
 const SEL_BTN_MORE = ".c-button .show-more-btn";
