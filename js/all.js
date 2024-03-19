1d05fb0212194d5dd3d1df8b7dbee0e56e56218f
Joe Johnston <104495+simple10@users.noreply.github.com>
2024-03-19 05:33:10 -0700

    v1.4.5

--- js/all.js
+++ js/all.js
@@ -117,7 +117,7 @@
 
   setCSSDefaults();
   showMore.init();
-  console.log(`"Level Up Theme" v: "1.4.4"`);
+  console.log(`"Level Up Theme" v: "1.4.5"`);
   console.log(`https://highlevelthemes.com`);
 
 })();
