aa1c10c0973f220af5c97cb4a5910d5d1b3bfc71
Joe Johnston <104495+simple10@users.noreply.github.com>
2024-03-20 12:45:39 -0700

    v1.5.0

--- js/all.js
+++ js/all.js
@@ -117,7 +117,7 @@
 
   setCSSDefaults();
   showMore.init();
-  console.log(`"Level Up Theme" v: "1.4.5"`);
+  console.log(`"Level Up Theme" v: "1.5.0"`);
   console.log(`https://highlevelthemes.com`);
 
 })();
--- js/components/defaults.js
+++ js/components/defaults.js
@@ -1,7 +1,7 @@
 /****************************************
  * Level Up Theme for High Level (GHL)
  * https//highlevelthemes.com
- * Version: v1.4.5
+ * Version: v1.5.0
  ****************************************/
 
 const themeVariablesDefaults = [
