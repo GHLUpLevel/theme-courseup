f08fa5072dce74f9f3cc4d67ddc3cf54df843bce
Joe Johnston <104495+simple10@users.noreply.github.com>
2024-03-26 12:53:09 -0700

    v1.6.4

--- js/components/defaults.js
+++ js/components/defaults.js
@@ -1,7 +1,7 @@
 /****************************************
  * Level Up Theme for High Level (GHL)
  * https//highlevelthemes.com
- * Version: v1.6.3
+ * Version: v1.6.4
  ****************************************/
 
 const themeVariablesDefaults = [
