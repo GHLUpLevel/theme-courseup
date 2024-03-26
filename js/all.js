7e7200f9264d2bea105318cf3987f5ff9777a964
Joe Johnston <104495+simple10@users.noreply.github.com>
2024-03-26 15:43:55 -0700

    v1.6.5

--- js/all.js
+++ js/all.js
@@ -129,7 +129,7 @@
 
   setCSSDefaults();
   showMore.init();
-  console.log(`"Level Up Theme" v: "1.6.4"`);
+  console.log(`"Level Up Theme" v: "1.6.5"`);
   console.log(`https://highlevelthemes.com`);
 
 })();
--- js/components/swiper.js
+++ js/components/swiper.js
@@ -1,7 +1,7 @@
 /****************************************
  * Level Up Theme for High Level (GHL)
  * https//highlevelthemes.com
- * Version: v1.6.4
+ * Version: v1.6.5
  ****************************************/
 
 console.warn("Swiper not yet implemented");
