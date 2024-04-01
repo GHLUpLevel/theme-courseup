4c6ecd11865612796637cb8cb78c3425b92aa237
Joe Johnston <104495+simple10@users.noreply.github.com>
2024-03-31 17:06:46 -0700

    v1.6.11

--- js/all.js
+++ js/all.js
@@ -36,7 +36,7 @@
     observer.observe(trigger.parentNode, { childList: true });
     addClickListeners(btnContainer, onClick);
   };
-  const run = () => {
+  const run$1 = () => {
     document.querySelectorAll(SEL_BTN_MORE).forEach((trigger) => {
       const btnContainer = trigger.closest("div.inner");
       if (!btnContainer) {
@@ -68,18 +68,18 @@
       }
     });
   };
-  const init = () => {
+  const init$1 = () => {
     if (document.readyState === "complete" || document.readyState === "interactive") {
-      setTimeout(run, 10);
+      setTimeout(run$1, 10);
     } else {
       document.addEventListener("DOMContentLoaded", () => {
-        setTimeout(run, 10);
+        setTimeout(run$1, 10);
       });
     }
   };
   var showMore = {
-    init,
-    run
+    init: init$1,
+    run: run$1
   };
 
   const themeVariablesDefaults = [
@@ -127,9 +127,67 @@
     });
   }
 
+  var __async = (__this, __arguments, generator) => {
+    return new Promise((resolve, reject) => {
+      var fulfilled = (value) => {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      };
+      var rejected = (value) => {
+        try {
+          step(generator.throw(value));
+        } catch (e) {
+          reject(e);
+        }
+      };
+      var step = (x) => x.done ? resolve(x.value) : Promise.resolve(x.value).then(fulfilled, rejected);
+      step((generator = generator.apply(__this, __arguments)).next());
+    });
+  };
+  const ANIMATE_SELECTOR = "[class*=animate-]";
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const elem = entry.target;
+        elem.classList.add(...elem.dataset.animate.split(","));
+        observer.unobserve(elem);
+      }
+    });
+  });
+  const processElem = (elem) => __async(void 0, null, function* () {
+    const animClasses = [];
+    elem.classList.forEach((cls) => {
+      if (cls.substring(0, 8) === "animate-") {
+        animClasses.push(cls);
+      }
+    });
+    elem.dataset.animate = animClasses.join(",");
+    elem.classList.remove(...animClasses);
+    observer.observe(elem);
+  });
+  const run = () => {
+    document.querySelectorAll(ANIMATE_SELECTOR).forEach((elem) => {
+      processElem(elem);
+    });
+  };
+  const init = () => __async(void 0, null, function* () {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+      setTimeout(run, 10);
+    } else {
+      document.addEventListener("DOMContentLoaded", () => {
+        setTimeout(run, 10);
+      });
+    }
+  });
+  var animate = { init };
+
   setCSSDefaults();
   showMore.init();
-  console.log(`"Level Up Theme" v: "1.6.10"`);
+  animate.init();
+  console.log(`"Level Up Theme" v: "1.6.11"`);
   console.log(`https://highlevelthemes.com`);
 
 })();
---
+++ js/components/animate.js
@@ -0,0 +1,65 @@
+/****************************************
+ * Level Up Theme for High Level (GHL)
+ * https//highlevelthemes.com
+ * Version: v1.6.11
+ ****************************************/
+
+var __async = (__this, __arguments, generator) => {
+  return new Promise((resolve, reject) => {
+    var fulfilled = (value) => {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    };
+    var rejected = (value) => {
+      try {
+        step(generator.throw(value));
+      } catch (e) {
+        reject(e);
+      }
+    };
+    var step = (x) => x.done ? resolve(x.value) : Promise.resolve(x.value).then(fulfilled, rejected);
+    step((generator = generator.apply(__this, __arguments)).next());
+  });
+};
+const ANIMATE_SELECTOR = "[class*=animate-]";
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const elem = entry.target;
+      elem.classList.add(...elem.dataset.animate.split(","));
+      observer.unobserve(elem);
+    }
+  });
+});
+const processElem = (elem) => __async(void 0, null, function* () {
+  const animClasses = [];
+  elem.classList.forEach((cls) => {
+    if (cls.substring(0, 8) === "animate-") {
+      animClasses.push(cls);
+    }
+  });
+  elem.dataset.animate = animClasses.join(",");
+  elem.classList.remove(...animClasses);
+  observer.observe(elem);
+});
+const run = () => {
+  document.querySelectorAll(ANIMATE_SELECTOR).forEach((elem) => {
+    processElem(elem);
+  });
+};
+const init = () => __async(void 0, null, function* () {
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+    setTimeout(run, 10);
+  } else {
+    document.addEventListener("DOMContentLoaded", () => {
+      setTimeout(run, 10);
+    });
+  }
+});
+var animate = { init };
+
+export { animate as default };
+//# sourceMappingURL=animate.js.map
