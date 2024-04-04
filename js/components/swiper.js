200e4c718658d01a57fe26b0ecaf80bfa924beea
Joe Johnston <104495+simple10@users.noreply.github.com>
2024-04-04 09:41:11 -0700

    v1.7.6

--- js/components/swiper.js
+++ js/components/swiper.js
@@ -1,7 +1,7 @@
 /****************************************
  * Level Up Theme for High Level (GHL)
  * https//highlevelthemes.com
- * Version: v1.7.5
+ * Version: v1.7.6
  ****************************************/
 
 console.warn("Swiper not yet implemented");
