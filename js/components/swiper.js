61a803b2a3ed921e2bec4dfa5a1784b53170d1ba
Joe Johnston <104495+simple10@users.noreply.github.com>
2024-04-08 12:53:05 -0700

    v1.7.16

--- js/components/swiper.js
+++ js/components/swiper.js
@@ -1,7 +1,7 @@
 /****************************************
  * Level Up Theme for High Level (GHL)
  * https//levelupthemes.com
- * Version: v1.7.15
+ * Version: v1.7.16
  ****************************************/
 
 console.warn("Swiper not yet implemented");
--- js/components/topnav.js
+++ js/components/topnav.js
@@ -1,7 +1,7 @@
 /****************************************
  * Level Up Theme for High Level (GHL)
  * https//levelupthemes.com
- * Version: v1.7.15
+ * Version: v1.7.16
  ****************************************/
 
 const POLYFILL_DELAY = 1e3;
