337766fd52d7bb65522d94500f16b302f5457676
Joe Johnston <104495+simple10@users.noreply.github.com>
2024-04-07 21:40:07 -0700

    v1.7.14

--- js/components/show-more.js
+++ js/components/show-more.js
@@ -1,7 +1,7 @@
 /****************************************
  * Level Up Theme for High Level (GHL)
  * https//levelupthemes.com
- * Version: v1.7.13
+ * Version: v1.7.14
  ****************************************/
 
 const SEL_BTN_MORE = ".show-more-btn";
--- js/components/swiper.js
+++ js/components/swiper.js
@@ -1,7 +1,7 @@
 /****************************************
  * Level Up Theme for High Level (GHL)
  * https//levelupthemes.com
- * Version: v1.7.13
+ * Version: v1.7.14
  ****************************************/
 
 console.warn("Swiper not yet implemented");
