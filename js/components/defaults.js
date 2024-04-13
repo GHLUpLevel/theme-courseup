07423dd786f697435ffbeaf957c94384060f8c3d
Joe Johnston <104495+simple10@users.noreply.github.com>
2024-04-12 17:41:44 -0700

    v1.7.23

--- js/components/defaults.js
+++ js/components/defaults.js
@@ -1,7 +1,7 @@
 /****************************************
  * Level Up Theme for High Level (GHL)
  * https//levelupthemes.com
- * Version: v1.7.22
+ * Version: v1.7.23
  ****************************************/
 
 const themeVariablesDefaults = [
